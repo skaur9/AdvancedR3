8b77685d817e7f0a5b8a5157b8325ebb23a43d56
Simranjeet Kaur <simranjeet84@gmail.com>
2024-12-10 16:19:46 +0100

    Add a function in functions.R

---
+++ R/functions.R
@@ -0,0 +1,20 @@
+#' Title "Decriptive statistics"
+#'
+#' @param data
+#'
+#' @return  “A data.frame/tibble.”
+#'
+descriptive_stats <- function(data) {
+    data %>% dplyr::group_by(metabolite) %>%
+        dplyr::summarise(across(
+            value,
+            list(
+                mean = mean,
+                sd = sd
+            )
+        )) %>%
+        dplyr::mutate(across(
+            where(is.numeric),
+            ~ round(.x, digits = 1)
+        ))
+}
